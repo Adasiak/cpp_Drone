bdefc31f574bc019decfaa0bd2c23f5074f488b0
Wiktor Adasiak stacjo ubuntu 22.04 <wadasiak01@gmail.com>
2026-03-19 18:37:37 +0100

    Remove build artifacts from tracking, add .gitignore, fix doctest SIGSTKSZ

--- build/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/boo.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/boo.dir/main.cpp.o: \
-  /Users/apple/Desktop/zad5_3-Adasiak/build/CMakeFiles/_CMakeLTOTest-CXX/src/main.cpp
--- build/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/foo.dir/foo.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/foo.dir/foo.cpp.o: \
-  /Users/apple/Desktop/zad5_3-Adasiak/build/CMakeFiles/_CMakeLTOTest-CXX/src/foo.cpp
